f5f0a1867a5b8f48dee55c422697e1852ad941a1
Rizal Andhika Prayoga <167048869+rzladk@users.noreply.github.com>
2024-05-25 07:44:29 +0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const menu = document.querySelector('#menu');
-const navbarNav = document.querySelector('.navbar-nav');    
-
-menu.addEventListener('click', function () {
-    navbarNav.classList.toggle('active');
-})
-
